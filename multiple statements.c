4e8c9811e18fbcb88930a1b5516dcf461a8b0328
meghanaandey7 <meghanaandey5@gmail.com>
2026-05-04 19:10:03 +0530

    Create multiple statements.c

---
+++ multiple statements.c
@@ -0,0 +1,10 @@
+/* write a program using multiple statements*/
+#include<stdio.h>
+int main()
+{
+printf("My name is Meghana\n");
+printf("Studying in ANITS\n");
+printf("Course: Biotechnology\n");
+printf("Roll no.: A25126506001\n");
+return 0;
+}
