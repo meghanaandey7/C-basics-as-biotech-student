f23fa4fdaa010d045c2c6b46869666101db0875a
meghanaandey7 <meghanaandey5@gmail.com>
2026-05-04 19:14:24 +0530

    Create addition of x&y.c

---
+++ addition of x&y.c
@@ -0,0 +1,10 @@
+/* addition of two numbers*/
+#include<stdio.h>
+int main()
+{
+int x,y;
+printf("enter the values of x and y: ");
+scanf("%d %d",&x,&y);
+printf("the sum of %d and %d is: %d",x,y,(x+y));
+return 0;
+}
